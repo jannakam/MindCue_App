56de1d0e258c7fb86e56cca287b372796532c7bd
Janna Almuqaisib <jannakalmuqaisib@gmail.com>
2023-08-25 15:29:37 +0300

    report screen

--- MindCue/screens/ReportScreen.js
+++ MindCue/screens/ReportScreen.js
@@ -1,12 +1,30 @@
-import { SafeAreaView} from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { SafeAreaView, View, Text, Button} from 'react-native';
+import { RadioButton } from 'react-native-paper';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import style from '../components/style';
 
 function ReportScreen({ navigation }) {
+    const [checked, setChecked] = React.useState('first');
     return (
         <SafeAreaView>
+            <View style={style.generalBox}>
+                <Text style={style.triggersTitle}>Report Generator</Text>
+                <View style={style.radiobuttonContainer}>
+                <RadioButton color='#DC989A' uncheckedColor= '#638184' value='Weekly' status={checked === 'first' ? 'checked' : 'unchecked'} onPress={() => setChecked('first')} />
+                <Text>Weekly</Text>
+                </View>
+                <View style={style.radiobuttonContainer}>
+                <RadioButton color='#DC989A' uncheckedColor= '#638184' value='Bimonthly' status={checked === 'second' ? 'checked' : 'unchecked'} onPress={() => setChecked('second')} />
+                <Text>Bimonthly</Text>
+                </View >
+                <View style={style.radiobuttonContainer}>
+                <RadioButton color='#DC989A' uncheckedColor= '#638184' value='Monthly' status={checked === 'third' ? 'checked' : 'unchecked'} onPress={() => setChecked('third')} />
+                <Text>Monthly</Text>
+                </View>
+                <Button style={style.button1} title='Download Report'/>
+            </View>
         </SafeAreaView>
         );
 }
--- MindCue/screens/TriggerScreen.js
+++ MindCue/screens/TriggerScreen.js
@@ -1,20 +1,60 @@
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, Button} from 'react-native';
 import * as React from 'react';
-import { TextInput, Checkbox } from 'react-native-paper';
+import { Checkbox, TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import style from '../components/style';
 
+function AddWordsItem(props) {
+const [inputWords, setWords] = React.useState('');
+const ClickedAddWords = () => {
+    props.wordsToAdd(inputWords)
+    setWords('')
+    }
+    return (
+        <><Text style={style.triggersTitle}>Keywords List</Text><TouchableOpacity onPress={ClickedAddWords}>
+            <Text>Add Triggers:</Text>
+        </TouchableOpacity><TextInput placeholder='ex: Guns' onChangeText={(result) => setWords(result)} value={inputWords}></TextInput></>
+);
+}
+
 function TriggerScreen({ navigation }) {
+    const [war, setWar] = React.useState(false);
+    const [childAbuse, setChildAbuse] = React.useState(false);
+    const [gore, setGore] = React.useState(false);
+    const [death, setDeath] = React.useState(false);
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+        
     return (
         <SafeAreaView>
-            <View style={style.triggers}>
+            <View style={style.generalBox}>
                 <Text style={style.triggersTitle}>Triggers List</Text>
-                <Checkbox label='War' status='checked' onPress={null}/>
-    
+                <View style={style.checkboxContainer}>
+                <Checkbox color='#DC989A' uncheckedColor='#638184' label='War' status={war ? 'checked' : 'unchecked'} onPress={() => {setWar(!war);}}/>
+                <Text>War</Text>
+                </View>
+                <View style={style.checkboxContainer}>
+                <Checkbox color='#DC989A' uncheckedColor='#638184' label='Child Abuse' status={childAbuse ? 'checked' : 'unchecked'} onPress={() => {setChildAbuse(!childAbuse);}}/>
+                <Text>Child Abuse</Text>
+                </View>
+                <View style={style.checkboxContainer}>
+                <Checkbox color='#DC989A' uncheckedColor='#638184' label='Gore' status={gore ? 'checked' : 'unchecked'} onPress={() => {setGore(!gore);}}/>
+                <Text>Gore</Text>
+                </View>
+                <View style={style.checkboxContainer}>
+                <Checkbox color='#DC989A' uncheckedColor='#638184' label='Death' status={death ? 'checked' : 'unchecked'} onPress={() => {setDeath(!death);}}/>
+                <Text>Death</Text>
+                </View>
+                {/* <SearchBar placeholder='Search for a trigger' onChangeText={onChangeSearch} value={searchQuery}/> */}
+            </View>
+            <View style={style.keywords}>
             </View>
         </SafeAreaView>
         );
-}
+
+    }
+
 
 export default TriggerScreen;
