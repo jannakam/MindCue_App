ec30f0d6fbff99c4080a8f7f760ec65ebb492780
Janna Almuqaisib <jannakalmuqaisib@gmail.com>
2023-09-14 17:48:24 +0300

    timer screen functionality

--- MindCue/screens/ScreenTimeScreen.js
+++ MindCue/screens/ScreenTimeScreen.js
@@ -5,7 +5,9 @@ import { TextInput } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import RNPickerSelect from 'react-native-picker-select';
 import UserNavbar from '../components/UserNavbar';
 import style from '../components/style';
 
@@ -127,11 +129,112 @@ function ScreenTimeScreen ({ navigation }) {
 // const formatDate = (time) => {
 //   return `${time.getHours()}:${time.getMinutes()}`;
 // };
-    return (
-        <SafeAreaView>
-            <View></View>
-        </SafeAreaView>
-    );
-}
+const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [selectedHours, setSelectedHours] = useState('');
+  const [selectedMinutes, setSelectedMinutes] = useState('');
+  const [remainingTime, setRemainingTime] = useState(0);
+
+  const timerRef = useRef(null);
+
+  const handleStartTimer = () => {
+    const hours = parseInt(selectedHours) || 0;
+    const minutes = parseInt(selectedMinutes) || 0;
+    const totalSeconds = hours * 3600 + minutes * 60;
+    setRemainingTime(totalSeconds);
+    setIsTimerRunning(true);
+
+    timerRef.current = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => {
+        if (prevRemainingTime <= 1) {
+          clearInterval(timerRef.current);
+          setIsTimerRunning(false);
+          return 0;
+        }
+        return prevRemainingTime - 1;
+      });
+    }, 1000);
+  };
+
+  const handlePauseTimer = () => {
+    clearInterval(timerRef.current);
+    setIsTimerRunning(false);
+  };
+
+  const renderTimer = () => {
+    if (isTimerRunning) {
+      const hours = Math.floor(remainingTime / 3600);
+      const minutes = Math.floor((remainingTime % 3600) / 60);
+      const seconds = remainingTime % 60;
+
+      return (
+        <Text style={styles.timerText}>
+          {`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
+        </Text>
+      );
+    } else {
+      return <Text style={styles.timerText}>00:00:00</Text>;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Hours"
+          value={selectedHours}
+          onChangeText={(text) => setSelectedHours(text)}
+          keyboardType="numeric"
+        />
+        <Text style={styles.separator}>:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Minutes"
+          value={selectedMinutes}
+          onChangeText={(text) => setSelectedMinutes(text)}
+          keyboardType="numeric"
+        />
+      </View>
+      {renderTimer()}
+      <View style={styles.buttonContainer}>
+        {!isTimerRunning ? (
+          <Button title="Start" onPress={handleStartTimer} />
+        ) : (
+          <Button title="Reset" onPress={handlePauseTimer} />
+        )}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  input: {
+    width: 60,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlign: 'center',
+  },
+  separator: {
+    fontSize: 24,
+    marginHorizontal: 10,
+  },
+  timerText: {
+    fontSize: 36,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    width: 100,
+  },
+});
 
 export default ScreenTimeScreen;
