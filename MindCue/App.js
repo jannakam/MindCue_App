ceff3a55866bb6c938e6667ce29c9b4cf1d56db6
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2023-09-29 16:01:38 +0300

    QrCode scanner done

--- MindCue/App.js
+++ MindCue/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {Text, View, SafeAreaView, ImageBackground, Button, useState} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {Text, View, SafeAreaView, ImageBackground, Button,StyleSheet, TouchableOpacity,Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { RadioButton, TextInput } from 'react-native-paper';
+import { ActivityIndicator, RadioButton, TextInput } from 'react-native-paper';
 import UserNavbar from './components/UserNavbar';
 import HomeScreen from './screens/HomeScreen';
 import UserVerification from './screens/UserVerification';
@@ -21,12 +21,12 @@ import Patient from './screens/Patient';
 import DoctorEdit from './screens/DoctorEdit';
 import DoctorChatScreen from './screens/DoctorChatScreen';
 import DoctorContacts from './screens/DoctorContacts';
+import AddPatient from './screens/AddPatient'
 import LogScreen from './screens/LogScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import EditScreen from './screens/EditScreen';
 import style from './components/style';
 import DoctorChat from './screens/DoctorChat';
-
 // This should be the background image for the sign in page
 // const image = { uri : './assets/images/Ellipse2.png'};
 
@@ -36,39 +36,38 @@ const Stack = createNativeStackNavigator();
 // This is the function that is going to list all our screens
 function App() {
   //const [isSignedIn, setIsSignedIn] = useState(false);
-  
   return (
-    <NavigationContainer>
-    <Stack.Navigator name='HomeScreen' initialRouteName={HomeScreen}>
-        <Stack.Screen name='UserNavbar' component={UserNavbar} options={{ headerShown: false}}/>
-        <Stack.Screen name='SignInScreen' component={SignInScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='SignUpScreen' component={SignUpScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='UserVerification' component={UserVerification} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A',}}/>
-        <Stack.Screen name='TriggerScreen' component={TriggerScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2',}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='ReportScreen' component={ReportScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='ScreenTimeScreen' component={ScreenTimeScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='QRScreen' component={QRScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='ChatScreen' component={ChatScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='LogScreen' component={LogScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='ProfileScreen' component={ProfileScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='EditScreen' component={EditScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        <Stack.Screen name='DoctorChat' component={DoctorChat} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-        </Stack.Navigator>
+    // <NavigationContainer>
+    // <Stack.Navigator name='HomeScreen' initialRouteName={HomeScreen}>
+    //     <Stack.Screen name='UserNavbar' component={UserNavbar} options={{ headerShown: false}}/>
+    //     <Stack.Screen name='SignInScreen' component={SignInScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='SignUpScreen' component={SignUpScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='UserVerification' component={UserVerification} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A',}}/>
+    //     <Stack.Screen name='TriggerScreen' component={TriggerScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2',}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='ReportScreen' component={ReportScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='ScreenTimeScreen' component={ScreenTimeScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='QRScreen' component={QRScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='ChatScreen' component={ChatScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='LogScreen' component={LogScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='ProfileScreen' component={ProfileScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='EditScreen' component={EditScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     <Stack.Screen name='DoctorChat' component={DoctorChat} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+    //     </Stack.Navigator>
+    //   </NavigationContainer>
+      <NavigationContainer>
+            <Stack.Navigator name='DoctorHome' initialRouteName={DoctorHome}>
+            <Stack.Screen name='DoctorNavbar' component={DoctorNavbar} options={{ headerShown: false }}/>
+            <Stack.Screen name='DoctorVerification' component={DoctorVerification} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='DoctorHome' component={DoctorHome} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='Patient' component={Patient} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='DoctorProfile' component={DoctorProfile} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='DoctorEdit' component={DoctorEdit} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='DoctorChatScreen' component={DoctorChatScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='AddPatient' component={AddPatient} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            <Stack.Screen name='DoctorContacts' component={DoctorContacts} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
+            </Stack.Navigator>
       </NavigationContainer>
-
-      // <NavigationContainer>
-      //       <Stack.Navigator name='DoctorHome' initialRouteName={DoctorHome}>
-      //       <Stack.Screen name='DoctorNavbar' component={DoctorNavbar} options={{ headerShown: false }}/>
-      //       <Stack.Screen name='DoctorVerification' component={DoctorVerification} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='DoctorHome' component={DoctorHome} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='Patient' component={Patient} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='DoctorProfile' component={DoctorProfile} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='DoctorEdit' component={DoctorEdit} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='DoctorChatScreen' component={DoctorChatScreen} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       <Stack.Screen name='DoctorContacts' component={DoctorContacts} options={{ headerStyle: {backgroundColor: '#f2f2f2'}, headerTitleStyle: {fontFamily: 'Poppins-SemiBold', color: '#638184'}, headerTintColor: '#DC989A', headerShadowVisible: false}}/>
-      //       </Stack.Navigator>
-      // </NavigationContainer>
       );
 }
 
--- MindCue/screens/DoctorHome.js
+++ MindCue/screens/DoctorHome.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { Text, View, SafeAreaView, ImageBackground} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {Text, View, SafeAreaView, ImageBackground, Button,StyleSheet, TouchableOpacity,Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import style from '../components/style';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Camera , useCameraDevices} from 'react-native-vision-camera'
+import { camelCase } from 'react-native-svg/lib/typescript/xml';
 
 function DoctorHome({ navigation }) {
+    const devices = Camera.getAvailableCameraDevices()
+    const device = devices.find((d) => d.position === 'back')
+    const camera = useRef(null)
+    const [imageData, setImageData] = useState('')
+    const [takePhotoClicked,setTakePhotoClicked ]=useState(false)
+     
+  
+    useEffect(()=>{
+      checkPeermission()
+    },[])
+    const checkPeermission =async()=>{
+      const newcameraPermission = await Camera.requestCameraPermission()
+      const newmicrophonePermission = await Camera.requestMicrophonePermission()
+      console.log(newcameraPermission)
+    }
+    if (device == null) return <ActivityIndicator />
+   const takePicture = async()=>{
+    if(camera!=null){
+      const photo = await camera.current.takePhoto()
+      setImageData(photo.path)
+      setTakePhotoClicked(false)
+  console.log(photo.path)
+    }
+   }
     return (
         <SafeAreaView>
             <ImageBackground style={style.userCircle} source={require('../assets/images/Ellipse15.png')}/>
@@ -24,6 +50,9 @@ function DoctorHome({ navigation }) {
                 <View style={style.dashboardContainer}>
                 <Text style={style.dashboardOptions} onPress={() => navigation.navigate('Patient')}>Janna Almuqaisib</Text>
                 <Icon name='arrow-right' size={20} color='#638184' style={{position: 'absolute', marginLeft: 183, top: 4}} onPress={() => navigation.navigate('Patient')}/>
+                </View>              
+                <View style={style.dashboardContainer}>
+                <Text style={style.dashboardOptions} onPress={() => navigation.navigate('AddPatient')}>Add a patient</Text>
                 </View>
             </View>
         </SafeAreaView>
